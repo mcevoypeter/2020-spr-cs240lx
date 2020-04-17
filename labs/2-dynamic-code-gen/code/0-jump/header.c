e986180f986ae6ae89eea163d740c2acd97299e6
Peter McEvoy <mcevoypeter@outlook.com>
2020-04-17 01:04:57 -0600

    [completed] 2-dynamic-code-gen part0
    Inserted code into `memmap.header` and pointed `header_str` to it, which
    allowed us to print arbitrary bytes at runtime.

--- labs/2-dynamic-code-gen/code/0-jump/header.c
+++ labs/2-dynamic-code-gen/code/0-jump/header.c
@@ -5,8 +5,7 @@ void notmain(void) {
     // print out the string in the header.
 
     // figure out where it points to!
-    unimplemented();
-    const char *header_string = 0;
+    const char *header_string = (const char *)0x8014;
 
     assert(header_string);
     printk("<%s>\n", header_string);
