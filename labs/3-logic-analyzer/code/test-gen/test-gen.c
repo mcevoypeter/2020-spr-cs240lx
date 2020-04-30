54e9389fd780c65952ca301f2fe5027d47d79028
Peter McEvoy <mcevoypeter@outlook.com>
2020-04-30 15:51:45 -0600

    [updated] 3-logic-analyzer
    Rolling the loop back up seemed to speed things up. I was consistently
    getting ~66090 cycles with `CYCLE_PER_FLIP` of 6000 with an completely
    unrolled loop (i.e. hardcoded number of delays), but switching to an
    infinite loop with a break statement bumped me down to ~66029 cycles.

--- labs/3-logic-analyzer/code/test-gen/test-gen.c
+++ labs/3-logic-analyzer/code/test-gen/test-gen.c
@@ -5,11 +5,9 @@
 
 #include "../scope-constants.h"
 
-extern void generate(unsigned pin, unsigned N, unsigned ncycle, unsigned start);
-
 static volatile unsigned *SET0 = (unsigned *)0x2020001c;
 static volatile unsigned *CLR0 = (unsigned *)0x20200028;
-static inline void fast_gpio_write(unsigned val, unsigned pin) {
+static inline void fast_gpio_write(unsigned pin, unsigned val) {
     if (val)
         *SET0 = 1 << 21;
     else
@@ -18,49 +16,20 @@ static inline void fast_gpio_write(unsigned val, unsigned pin) {
 
 // send N samples at <ncycle> cycles each in a simple way.
 void test_gen(unsigned pin, unsigned N, unsigned ncycle) {
-    // I'm always 250 cycles too high with this approach. Why?
-#if 0
     enable_cache();
-    unsigned start = cycle_cnt_read();
-    generate(pin, N, ncycle, start);
-    unsigned end = cycle_cnt_read();
-#endif
-    enable_cache();
+    unsigned val = 1;
+    unsigned end = N*ncycle;
     unsigned cycles = ncycle;
     unsigned start = cycle_cnt_read();
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(0, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(0, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(0, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(0, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(0, pin);
-    cycles += ncycle;
-    while (cycle_cnt_read() - start < cycles);
-    fast_gpio_write(1, pin);
-    unsigned end = cycle_cnt_read();
+    for (;;) {
+        fast_gpio_write(pin, val); 
+        while (cycle_cnt_read() - start < cycles);
+        val = 1-val;
+        cycles += ncycle;
+        if (cycles > end)
+            break;
+    }
+    end = cycle_cnt_read();
 
     // crude check how accurate we were ourselves.
     printk("expected %d cycles, have %d\n", ncycle*N, end-start);
