4375677911b7904872b743ebfeaa959b2c7fd1b9
Peter McEvoy <mcevoypeter@outlook.com>
2020-05-05 19:12:58 -0600

    [updated] 4-uart-8n1 sw-uart
    Added `sw_uart_put32` and `sw_uart_get32` to `sw-uart.c`. Both appear to
    be working. Next step: add parallel pins.

--- labs/4-uart-8n1/code/sw-uart/sw-uart.c
+++ labs/4-uart-8n1/code/sw-uart/sw-uart.c
@@ -16,108 +16,61 @@ typedef struct my_sw_uart my_sw_uart_t;
 int sw_uart_get8(my_sw_uart_t *uart) {
     // Assume that `cycle_cnt_init` has already been called.
 
+    unsigned rx = uart->rx;
     int b = 0;
-
-    unsigned cum_cycles = CYCLES_PER_BIT + CYCLES_PER_BIT/2;
-    unsigned start = cycle_cnt_read();
-
-    // Wait for start bit.
-    while (fast_gpio_read(uart->rx) != 0);
     
-    // Delay `CYCLES_PER_BIT + CYCLES_PER_BIT/2` cycles.
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
+    // Wait for start bit.
+    while (fast_gpio_read(rx) != 0);
+    delay_ncycles(cycle_cnt_read(), CYCLES_PER_BIT + CYCLES_PER_BIT/2);
 
-    b |= fast_gpio_read(uart->rx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 1;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 2;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 3;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 4;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 5;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 6;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    b |= fast_gpio_read(uart->rx) << 7;
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
+    for (unsigned i = 0; i < 8; i++) {
+        b |= (fast_gpio_read(rx) << i);
+        delay_ncycles(cycle_cnt_read(), CYCLES_PER_BIT);
+    }
 
     // Wait for stop bit.
-    /*while (fast_gpio_read(uart->rx) != 1);*/
+    while (fast_gpio_read(rx) != 1);
 
     return b;
 }
 
 
 int sw_uart_put8(my_sw_uart_t *uart, uint8_t b) {
-    // Assume that `cycle_cnt_init` has already been called.
-
-    unsigned cum_cycles = CYCLES_PER_BIT;
     unsigned tx = uart->tx;
-    unsigned start = cycle_cnt_read();
 
     // Write start bit for `CYCLES_PER_BIT` cycles.
     fast_gpio_write(0, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
+    delay_ncycles(cycle_cnt_read(), CYCLES_PER_BIT);
 
-    fast_gpio_write(b & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 1) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 2) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 3) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles); 
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 4) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 5) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 6) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
-
-    fast_gpio_write((b >> 7) & 1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
-    cum_cycles += CYCLES_PER_BIT;
+    for (unsigned i = 0; i < 8; i++) {
+        fast_gpio_write((b >> i) & 1, tx);
+        delay_ncycles(cycle_cnt_read(), CYCLES_PER_BIT);
+    }
 
     // Write stop bit for `CYCLES_PER_BIT` cycles.
     fast_gpio_write(1, tx);
-    while (cycle_cnt_read() - start < cum_cycles);
+    delay_ncycles(cycle_cnt_read(), CYCLES_PER_BIT);
 
-    unsigned end = cycle_cnt_read();
     return 0;
 }
+
+
+int sw_uart_get32(my_sw_uart_t *uart) {
+    return sw_uart_get8(uart) 
+        | sw_uart_get8(uart) << 8
+        | sw_uart_get8(uart) << 16
+        | sw_uart_get8(uart) << 24;
+}
+
+
+int sw_uart_put32(my_sw_uart_t *uart, uint32_t w) {
+    for (unsigned i = 0; i < 32; i += 8)
+        sw_uart_put8(uart, (w >> i) & 0xff);
+    
+    return 0;
+}
+
+#define WORD 800
 
 // 5 second timeout
 unsigned timeout = 5 * 1000 * 1000;
@@ -140,22 +93,11 @@ static void server(unsigned tx, unsigned rx, unsigned n) {
         panic("timeout waiting for %d!\n", expected);
 
     for(unsigned i = 0; i < n; i++) {
-        sw_uart_put8(&server_uart, i);
-        bytes[i] = sw_uart_get8(&server_uart);
-#if 0
-        sw_uart_put8(&server_uart, i & 0xff);
-        sw_uart_put8(&server_uart, (i >> 8) & 0xff);
-        sw_uart_put8(&server_uart, (i >> 16) & 0xff);
-        sw_uart_put8(&server_uart, (i >> 24) & 0xff);
-        bytes[i] = sw_uart_get8(&server_uart);
-        bytes[i] |= sw_uart_get8(&server_uart) << 8;
-        bytes[i] |= sw_uart_get8(&server_uart) << 16;
-        bytes[i] |= sw_uart_get8(&server_uart) << 24;
-#endif
+        sw_uart_put32(&server_uart, i);
+        bytes[i] = sw_uart_get32(&server_uart);
     }
-    for (unsigned i = 0; i < n; i++) {
+    for (unsigned i = 0; i < n; i++) 
         printk("%u: server read %u\n", i, bytes[i]);
-    }
     printk("server done\n");
 }
 
@@ -174,18 +116,8 @@ static void client(unsigned tx, unsigned rx, unsigned n) {
 
     fast_gpio_write(1, client_uart.tx);
     for (unsigned i = 0; i < n; i++) {
-        bytes[i] = sw_uart_get8(&client_uart);
-        sw_uart_put8(&client_uart, i);
-#if 0
-        bytes[i] = sw_uart_get8(&client_uart);
-        bytes[i] |= sw_uart_get8(&client_uart) << 8;
-        bytes[i] |= sw_uart_get8(&client_uart) << 16;
-        bytes[i] |= sw_uart_get8(&client_uart) << 24;
-        sw_uart_put8(&client_uart, i & 0xff);
-        sw_uart_put8(&client_uart, (i >> 8) & 0xff);
-        sw_uart_put8(&client_uart, (i >> 16) & 0xff);
-        sw_uart_put8(&client_uart, (i >> 24) & 0xff);
-#endif
+        bytes[i] = sw_uart_get32(&client_uart);
+        sw_uart_put32(&client_uart, i);
     }
     for (unsigned i = 0; i < n; i++)
         printk("%u: client read %u\n", i, bytes[i]);
@@ -194,7 +126,6 @@ static void client(unsigned tx, unsigned rx, unsigned n) {
 }
 
 
-// send N samples at <ncycle> cycles each in a simple way.
 static void ping_pong(unsigned tx, unsigned rx, unsigned n) {
     if(!gpio_read(rx))
         server(tx,rx,n);
@@ -206,6 +137,6 @@ static void ping_pong(unsigned tx, unsigned rx, unsigned n) {
 void notmain(void) {
     enable_cache();
     cycle_cnt_init();
-    ping_pong(21,20,256);
+    ping_pong(21,20,4096);
     clean_reboot();
 }
