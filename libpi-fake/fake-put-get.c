1d30a5242589d3f33f8f8b720ca580cee92a67d6
Peter McEvoy <mcevoypeter@outlook.com>
2020-05-07 16:15:09 -0600

    [updated] libpi-fake
    Added a check in `get32` for the address of the timer register and
    accompanying logic.

--- libpi-fake/fake-put-get.c
+++ libpi-fake/fake-put-get.c
@@ -9,11 +9,14 @@ unsigned get32(const volatile void *addr) {
     // you won't need this if you write fake versions of the timer code.
     // however, if you link in the raw timer.c from rpi, thne you'll need
     // to recognize the time address and returns something sensible.
-    trace("GET32(%p)\n", addr);
-    /*if ((uintptr_t)addr == 0x20003004) {*/
-
-    /*}*/
-    return 0;
+    if ((uintptr_t)addr == 0x20003004) {
+        fake_time_inc(1);
+        trace("GET32(%p)=0x%x\n", addr, fake_time_usec);
+        return fake_time_usec;
+    }
+    unsigned u = fake_random();
+    trace("GET32(%p)=0x%x\n", addr, u);
+    return u;
 }
 
 // for today: simply print (addr,val) using trace()
