98a1af3c25e798ba3b531889a31d20cb132c6ce8
Peter McEvoy <mcevoypeter@outlook.com>
2020-05-07 16:29:02 -0600

    [updated] symlink redundant files
    There are multiple copies of `hc-sr04.c`, `hc-sr04.h`, `sonar.c`, and
    `gpio-pud.c` lying around. Make each file exist in only one location
    (`labs/7-sonar/code` for the first three; `liblxpi/` for `gpio-pud.c`)
    and symlink the other locations to the one master location for each
    respective file.

--- labs/6-fake/code/gpio-pud.c
+++ labs/6-fake/code/gpio-pud.c
@@ -1,6 +1,1 @@
-// put your pull-up/pull-down implementations here.
-#include "rpi.h"
-
-void gpio_set_pullup(unsigned pin) { unimplemented(); }
-void gpio_set_pulldown(unsigned pin) { unimplemented(); }
-void gpio_pud_off(unsigned pin) { unimplemented(); }
+/Users/petermcevoy/cs240lx/liblxpi/gpio-pud.c
