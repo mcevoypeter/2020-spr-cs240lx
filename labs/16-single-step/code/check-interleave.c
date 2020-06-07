bcc07ddf9dc8a9965e1083b959a70018ab4453cd
Peter McEvoy <mcevoypeter@outlook.com>
2020-06-07 16:11:26 -0600

    [in-progress] 16-single-step part2
    Working on test6. syslock appears to be working, but now I'm getting an
    undefined instruction exception.

--- labs/16-single-step/code/check-interleave.c
+++ labs/16-single-step/code/check-interleave.c
@@ -58,6 +58,9 @@ static unsigned check_one_cswitch(checker_t *c) {
         // use <user_trampoline_ret> to start running c->A
         user_trampoline_ret(cpsr_user, (void (*)(void *))c->A, c);
 
+        if (!c->switched_p)
+            c->B(c);
+
         if(!c->check(c)) {
             output("ERROR: check failed when switched on address %p, after %d instructions\n",
                 c->switch_addr, c->switch_on_inst_n);
